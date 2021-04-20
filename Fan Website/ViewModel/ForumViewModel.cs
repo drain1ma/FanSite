073b5373378cb6c520a118ce21e5c31886d7b06a
drain1ma <drain1ma@cmich.edu>
2021-04-19 22:53:46 -0400

    Required fields work now

--- Fan Website/ViewModel/ForumViewModel.cs
+++ Fan Website/ViewModel/ForumViewModel.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Fan_Website.ViewModel
 {
     public class ForumViewModel
-    { 
+    {
+        [Required(ErrorMessage = "Please enter a title.")]
         public string PostTitle { get; set; }
-        public string UserName { get; set; }
+        [Required(ErrorMessage = "Please enter a description.")]
         public string Description { get; set; }
         public DateTime? CreatedOn { get; set; }
-
+        public string UserName { get; set; }
         public string Slug =>
             PostTitle?.Replace(' ', '-').ToLower();
     }
--- Fan Website/ViewModel/PostViewModel.cs
+++ Fan Website/ViewModel/PostViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace Fan_Website.ViewModel
 {
     public class PostViewModel
     {
+        [Required(ErrorMessage = "Please enter a title.")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Please enter content.")]
         public string Content { get; set; }
         public DateTime CreatedOn { get; set; }
         public string UserName { get; set; }
