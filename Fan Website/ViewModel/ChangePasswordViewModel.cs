c1c2ecf81f27989b7837e351ad439d7b384d7474
drain1ma <drain1ma@cmich.edu>
2021-04-21 22:46:48 -0400

    Users can change their password now

--- Fan Website/ViewModel/ChangePasswordViewModel.cs
+++ Fan Website/ViewModel/ChangePasswordViewModel.cs
@@ -10,14 +10,16 @@ namespace Fan_Website.ViewModel
     {
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "Current Password")]
         public string Password { get; set; }
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
         public string NewPassword { get; set; }
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password",
-            ErrorMessage = "Password and confirmation password do not match.")]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword",
+            ErrorMessage = "The new password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
 }
