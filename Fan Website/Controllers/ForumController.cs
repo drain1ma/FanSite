c1ea51c3704791f3a1e6ad3cbbdb74c13a4306ec
Matthew Drain <drain1ma@cmich.edu>
2021-04-30 23:28:13 -0400

    Username now shows for posts

--- Fan Website/Controllers/ForumController.cs
+++ Fan Website/Controllers/ForumController.cs
@@ -49,6 +49,7 @@ namespace Fan_Website.Controllers
                 Id = post.PostId,
                 AuthorId = post.User.Id,
                 AuthorRating = post.User.Rating,
+                AuthorName = post.User.UserName, 
                 Title = post.Title,
                 DatePosted = post.CreatedOn.ToString(),
                 RepliesCount = post.Replies.Count(),
