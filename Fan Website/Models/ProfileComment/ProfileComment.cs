8ba255c8d01ff7bb09fb403bec6464ddf3565918
drain1ma <drain1ma@cmich.edu>
2021-05-28 21:22:48 -0400

    Added profile comments

---
+++ Fan Website/Models/ProfileComment/ProfileComment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fan_Website.Models.ProfileComment
+{
+    public class ProfileComment
+    {
+    }
+}
